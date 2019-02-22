08189d2bc1d606f09600e6106a70f03592cfdc4a
songwell1024 <zySong0709@Foxmail.com>
2019-02-22 21:44:56 +0800

    add spit model

---
+++ communitygroup_spit/src/main/java/com/communitygroup/spit/SpitApplication.java
@@ -0,0 +1,20 @@
+package com.communitygroup.spit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @program:
+ * @description:
+ * @author: Song
+ * @create: Created in 2019-02-22 21:40
+ * @Modified by:
+ **/
+@SpringBootApplication
+public class SpitApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SpitApplication.class, args);
+    }
+
+}
