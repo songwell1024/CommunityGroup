83df73907f4110df72320284ff0e7d553b4a814b
songwell1024 <zySong0709@Foxmail.com>
2019-03-04 21:55:22 +0800

    add friend model

---
+++ communitygroup_friend/src/main/java/com/communitygroup/friend/FriendApplication.java
@@ -0,0 +1,33 @@
+package com.communitygroup.friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+import util.JwtUtil;
+
+/**
+ * @program:
+ * @description:
+ * @author: Song
+ * @create: Created in 2019-02-28 20:15
+ * @Modified by:
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class FriendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FriendApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker();
+    }
+
+    @Bean
+    public JwtUtil jwtUtil(){
+        return new JwtUtil();
+    }
+}
---
+++ communitygroup_friend/src/main/java/com/communitygroup/friend/pojo/NoFriend.java
@@ -0,0 +1,40 @@
+package com.communitygroup.friend.pojo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+/**
+ * @program:
+ * @description:
+ * @author: Song
+ * @create: Created in 2019-02-28 20:23
+ * @Modified by:
+ **/
+@Entity
+@Table(name="tb_nofriend")
+@IdClass(NoFriend.class)    //表示联合主键，添加也可以，不添加也可以
+public class NoFriend {
+    @Id
+    private String userid;
+    @Id
+    private String friendid;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getFriendid() {
+        return friendid;
+    }
+
+    public void setFriendid(String friendid) {
+        this.friendid = friendid;
+    }
+}
